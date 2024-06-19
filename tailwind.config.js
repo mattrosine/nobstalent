94f724ba2ef80b6c255e933f4fc181a019f40ebf
mattrosine <mattrosine@gmail.com>
2024-06-19 00:39:19 -0400

    Initial commit with DaisyUI and Flowbite integration

---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // This tells Tailwind to apply dark mode using a class
+  content: [
+    "/Users/Matt/Documents/Website builds/daisyui_testing/**/*.html", // This path covers all HTML files within the directory
+    "./node_modules/flowbite/**/*.js" // Add this line for Flowbite
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af",
+          "900": "#1e3a8a",
+          "950": "#172554"
+        }
+      },
+      fontFamily: {
+        'body': [
+          'Inter', 
+          'ui-sans-serif', 
+          'system-ui', 
+          '-apple-system', 
+          'system-ui', 
+          'Segoe UI', 
+          'Roboto', 
+          'Helvetica Neue', 
+          'Arial', 
+          'Noto Sans', 
+          'sans-serif', 
+          'Apple Color Emoji', 
+          'Segoe UI Emoji', 
+          'Segoe UI Symbol', 
+          'Noto Color Emoji'
+        ],
+        'sans': [
+          'Inter', 
+          'ui-sans-serif', 
+          'system-ui', 
+          '-apple-system', 
+          'system-ui', 
+          'Segoe UI', 
+          'Roboto', 
+          'Helvetica Neue', 
+          'Arial', 
+          'Noto Sans', 
+          'sans-serif', 
+          'Apple Color Emoji', 
+          'Segoe UI Emoji', 
+          'Segoe UI Symbol', 
+          'Noto Color Emoji'
+        ]
+      }
+    }
+  },
+  plugins: [
+    require('daisyui'), 
+    require('flowbite/plugin')
+  ],
+}
